57aa2e15f38573c17c6ba28c4322fa81f61171e7
Felipe Batista <feliperodriguesbatista@outlook.com>
2025-03-22 16:15:03 -0300

    Added tailwindcss

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,12 @@
 export default function Header() {
   return (
     <header>
-      <img src="https://www.example.com/logo.png" alt="Logo" />
-      <h1>My App</h1>
+      <img
+        src="https://www.example.com/logo.png"
+        alt="Logo"
+        className="text-red-200 bg-red-500"
+      />
+      <h1>My Asssspp</h1>
     </header>
   );
 }
